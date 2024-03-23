c91282481ffb498df0d81b377008f78acc788968
Dan Kim <kimd345@gmail.com>
2024-03-23 17:32:10 -0400

    trying to hack pqr's backend to fetch scroll data lol

--- app/(app)/two.tsx
+++ app/(app)/two.tsx
@@ -1,12 +1,29 @@
-import { Text } from '@/components/Themed';
+import { useQuery } from '@tanstack/react-query';
 import { Link, Tabs } from 'expo-router';
 
+import { Text, View } from '@/components/Themed';
+
 export default function SecondPage() {
+  const query = useQuery({
+    queryKey: ['scroll'],
+    queryFn: async () => {
+      const response = await fetch('http://localhost:8080/dex/load/t3g/c0f');
+      return response.json();
+    },
+  });
+
+  console.log(query.failureReason);
+
   return (
-    //     <Stack>
-    //   <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-    //   <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
-    // </Stack>
-    <Link href="/">App Layout</Link>
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}>
+      <Text>{query.data}</Text>
+      <Link href="/(app)">Go to the first page</Link>
+      <Tabs />
+    </View>
   );
 }
